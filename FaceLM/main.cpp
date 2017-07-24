3b0fb7b3258fd7db717fa0cb8340e5ef2ca40037
quanpan <quanpan302@hotmail.com>
2017-07-24 15:19:40 +0200

    Initial Commit

---
+++ FaceLM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FaceLM
+//
+//  Created by quan pan on 7/24/17.
+//  Copyright © 2017 DssFactory. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
